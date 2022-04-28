131fe0ca6bef0d8d52afeb83b02e8aaedbd2df20
daiki0508 <oochani05085@outlook.jp>
2022-04-28 18:24:59 +0900

    feat: Add basic layout.

--- app/src/main/java/com/websarva/wings/android/insecureshoppoc/ui/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/insecureshoppoc/ui/MainActivity.kt
@@ -3,10 +3,18 @@ package com.websarva.wings.android.insecureshoppoc.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.websarva.wings.android.insecureshoppoc.R
+import com.websarva.wings.android.insecureshoppoc.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(this.root)
+        }
     }
 }
